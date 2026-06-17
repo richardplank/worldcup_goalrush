7b1339c1e1beafa6ab71019c7682e5b917b4fa6e
richardplank <richardplank@hotmail.com>
2026-06-17 06:48:28 +0100

    Auto-update scores

--- wc_goalrush_app.R
+++ wc_goalrush_app.R
@@ -19,7 +19,7 @@ load(file = "lib/entries")
 # set entry masking on (1) or off
 masking <- 0
 runs <- 40
-#runs <- 1
+runs <- 1
 sleepytime <- if_else(runs == 1, 1, 120)
 
 # De-authorize public sheet
